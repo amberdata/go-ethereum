12afb86ff74b6712adb87c56c5e35bae4654e289
Peng Wang <pengwang@Pengs-Mac-mini.local>
2018-04-18 21:59:27 -0700

    add more logs

--- core/state_processor.go
+++ core/state_processor.go
@@ -98,8 +98,8 @@ func (p *StateProcessor) Process(block *types.Block, statedb *state.StateDB, cfg
 			common.CheckErr(err, nil)
 			fmt.Printf("maxBlockNumber = %d\n", maxBlockNumber)
 			fmt.Printf("block.NumberU64() = %d\n", block.NumberU64())
-			fmt.Printf("maxBlockNumber-block.NumberU64() = %d\n", maxBlockNumber-block.NumberU64())
-			if maxBlockNumber-block.NumberU64() <= 6 {
+			fmt.Printf("block.NumberU64()+6 = %d\n", block.NumberU64()+6)
+			if maxBlockNumber <= block.NumberU64()+6 {
 				fmt.Println("go to sleep")
 				time.Sleep(time.Duration(db.BlockTime) * time.Second)
 			} else {
